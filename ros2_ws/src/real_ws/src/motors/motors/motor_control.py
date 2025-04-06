1f6ebb896cb554e1ebfeaad557210d9f811cd050
pj0620 <pjm7748@g.rit.edu>
2025-04-06 14:47:27 -0400

    update motor control node

--- ros2_ws/src/real_ws/src/motors/motors/motor_control.py
+++ ros2_ws/src/real_ws/src/motors/motors/motor_control.py
@@ -11,12 +11,25 @@ class MotorControlService(Node):
         self.serial_port = serial.Serial(serial_port, 115200)
         self.srv = self.create_service(MotorControl, 'motor_control', self.motor_control_callback)
         self.get_logger().info('Service is ready to send mototr control commands')
+        
+    def get_speed_byte(self, speed: float) -> bytes:
+      # Clamp and convert to int
+      value = max(-127, min(127, int(round(speed * 127))))
+      
+      sign_bit = 0x80 if value < 0 else 0x00  # Set the S bit (MSB)
+      magnitude = abs(value) & 0x7F           # Ensure only lower 7 bits
+      encoded_byte = sign_bit | magnitude     # Combine S and XXXXXXX
+      
+      return bytes([encoded_byte])
 
     def motor_control_callback(self, request, response):
         self.get_logger().info(f'Received request: {request.motor_speeds}')
         speeds = request.motor_speeds
-        serial_command = f"SET_VS {speeds.velocity_left_1},{speeds.velocity_left_2},{speeds.velocity_right_1},{speeds.velocity_right_2}\n"
-        self.serial_port.write(serial_command.encode('utf-8'))
+        # serial_command = f"V{speeds.velocity_left_1},{speeds.velocity_left_2},{speeds.velocity_right_1},{speeds.velocity_right_2}\n"
+        serial_command = b"V"
+        for vel in [speeds.velocity_left_1, speeds.velocity_left_2, speeds.velocity_right_1, speeds.velocity_right_2]:
+          serial_command += self.get_speed_byte(vel)
+        self.serial_port.write(serial_command)
         return response
 
 def main(args=None):
