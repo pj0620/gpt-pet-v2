3dcdf9ab1575bdbd9f04b18b3a52537492a3c766
pj0620 <pjm7748@g.rit.edu>
2025-04-07 19:34:42 -0400

    return actual speeds

--- ros2_ws/src/real_ws/src/motors/motors/motor_control.py
+++ ros2_ws/src/real_ws/src/motors/motors/motor_control.py
@@ -3,13 +3,14 @@ from rclpy.node import Node
 from gptpet_common.msg import Velocities
 from gptpet_common.srv import MotorControl, MotorSpeed
 import serial
+import time
 
 class MotorControlService(Node):
     def __init__(self):
         super().__init__('motor_control_service')
         self.declare_parameter('serial_port', '/dev/ttyACM0')
         serial_port = self.get_parameter('serial_port').get_parameter_value().string_value
-        self.serial_port = serial.Serial(serial_port, 115200)
+        self.serial_port = serial.Serial(serial_port, 115200, timeout=1)
         self.srv = self.create_service(MotorControl, 'motor_control', self.motor_control_callback)
         self.srv = self.create_service(MotorSpeed, 'motor_speed', self.motor_speed_callback)
         self.get_logger().info('Service is ready to send mototr control commands')
@@ -23,6 +24,12 @@ class MotorControlService(Node):
       encoded_byte = sign_bit | magnitude     # Combine S and XXXXXXX
       
       return bytes([encoded_byte])
+    
+    def byte_to_float(self, b: int) -> bytes:
+      val = 2.0 * float(b[0] & 0x7F) / 127.0
+      if b[0] & 0x80:
+        val = -val
+      return val
 
     def motor_control_callback(self, request, response):
         self.get_logger().info(f'Received request: {request.motor_speeds}')
@@ -35,12 +42,22 @@ class MotorControlService(Node):
     
     def motor_speed_callback(self, request, response):
       self.get_logger().info(f'Received speed request')
-      response.motor_speeds = Velocities(
-        velocity_left_1=1.0,
-        velocity_left_2=2.0,
-        velocity_right_1=3.0,
-        velocity_right_2=4.0
-      )
+      speeds = self.serial_port.readline()
+      if speeds == b'':
+        self.get_logger().error(f'No serial data recieved!')
+        response.motor_speeds = Velocities(
+          velocity_left_1=0.0,
+          velocity_left_2=0.0,
+          velocity_right_1=0.0,
+          velocity_right_2=0.0
+        )
+      else:
+        response.motor_speeds = Velocities(
+          velocity_left_1=self.byte_to_float(speeds[1]),
+          velocity_left_2=self.byte_to_float(speeds[2]),
+          velocity_right_1=self.byte_to_float(speeds[3]),
+          velocity_right_2=self.byte_to_float(speeds[4])
+        )
       return response
 
 def main(args=None):
