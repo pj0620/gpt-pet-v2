37326da599ac4f04788f3c2bee005b0a4df1c593
pj0620 <pjm7748@g.rit.edu>
2025-05-10 00:00:43 -0400

    update launch file real ws

--- ros2_ws/src/real_ws/src/startup/launch/client.launch.py
+++ ros2_ws/src/real_ws/src/startup/launch/client.launch.py
@@ -36,12 +36,15 @@ def generate_launch_description():
   
   actions.append(
     launch_ros.actions.Node(
-      package='ros_icm20948',
-      executable='talker.py',
-      name='ros_icm20948',
-      respawn=True,
-      respawn_delay=2
-    ),
+      package="ros2_icm20948",
+      executable="icm20948_node",
+      name="icm20948_node",
+      parameters=[
+        {"i2c_address": 0x69},
+        {"frame_id": "imu_icm20948"},
+        {"pub_rate": 50},
+      ],
+    )
   )
 
   return launch.LaunchDescription(actions)
