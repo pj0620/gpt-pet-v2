1ad7a5c79e8348bb85c84b39c43c345a97b698ce
pj <pjm7748@g.rit.edu>
2025-05-09 22:13:03 -0400

    add submodules

--- bin/motors/custom.sh
+++ bin/motors/custom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ros2 service call /motor_control common_interfaces/srv/MotorControl "{motor_speeds: {velocity_left_1: 0.0, velocity_left_2: 0.0, velocity_right_1: 0.0, velocity_right_2: 0.7}}"
+ros2 service call /motor_control_service gptpet_common/srv/MotorControl "{motor_speeds: {velocity_left_1: -0.7, velocity_left_2: -0.7, velocity_right_1: -0.7, velocity_right_2: -0.7}}"
---
+++ bin/setup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ./ros2_ws/src/common/install/setup.bash
+source ./ros2_ws/src/real_ws/install/setup.bash
