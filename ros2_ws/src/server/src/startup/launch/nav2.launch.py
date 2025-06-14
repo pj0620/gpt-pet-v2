70fb09ca95eebbd7fa16ef76e24eb6d066445e86
pj <pjm7748@g.rit.edu>
2025-06-13 21:30:10 -0400

    urdf + ros_control

--- ros2_ws/src/server/src/startup/launch/nav2.launch.py
+++ ros2_ws/src/server/src/startup/launch/nav2.launch.py
@@ -7,12 +7,12 @@ from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pkg = get_package_share_directory('gptpet_navigation')
+    pkg = get_package_share_directory('startup')
 
     # allow overriding via CLI: `ros2 launch bringup.launch.py urdf_file:=...`
     urdf_launch_arg = DeclareLaunchArgument(
         'urdf_file',
-        default_value=os.path.join(pkg, 'urdf', 'bot.urdf'),
+        default_value=os.path.join(pkg, 'urdf', 'gptpet.urdf'),
         description='Full path to robot urdf file'
     )
 
@@ -31,30 +31,30 @@ def generate_launch_description():
         ),
 
         # 2) ros2_control_node: loads hardware + controllers.yaml
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            name='controller_manager',
-            output='screen',
-            parameters=[
-                {'robot_description': robot_description},
-                os.path.join(pkg, 'config', 'controllers.yaml')
-            ]
-        ),
+        # Node(
+        #     package='controller_manager',
+        #     executable='ros2_control_node',
+        #     name='controller_manager',
+        #     output='screen',
+        #     parameters=[
+        #         {'robot_description': robot_description},
+        #         os.path.join(pkg, 'config', 'controllers.yaml')
+        #     ]
+        # ),
 
         # 3) spawn joint_state_broadcaster
-        Node(
-            package='controller_manager',
-            executable='spawner.py',
-            arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
-            output='screen'
-        ),
+        # Node(
+        #     package='controller_manager',
+        #     executable='spawner',
+        #     arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+        #     output='screen'
+        # ),
 
-        # 4) spawn your mecanum drive controller
-        Node(
-            package='controller_manager',
-            executable='spawner.py',
-            arguments=['mecanum_drive_controller', '--controller-manager', '/controller_manager'],
-            output='screen'
-        ),
+        # # 4) spawn your mecanum drive controller
+        # Node(
+        #     package='controller_manager',
+        #     executable='spawner',
+        #     arguments=['mecanum_drive_controller', '--controller-manager', '/controller_manager'],
+        #     output='screen'
+        # ),
     ])
