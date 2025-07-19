4c66f5e8b1646d2ba3a108f8b38c4d2740fae11a
pj <pjm7748@g.rit.edu>
2025-07-19 15:45:02 -0400

    Update robot_state_publisher parameters in nav2.launch.py

--- ros2_ws/src/bot_ws/src/startup/launch/nav2.launch.py
+++ ros2_ws/src/bot_ws/src/startup/launch/nav2.launch.py
@@ -66,13 +66,13 @@ def generate_launch_description():
         # slam_toolbox,
 
         # 1) robot_state_publisher: publishes base_link→base_laser + other static TFs
-        # Node(
-        #     package='robot_state_publisher',
-        #     executable='robot_state_publisher',
-        #     name='robot_state_publisher',
-        #     output='screen',
-        #     parameters=[{'robot_description': robot_description}]
-        # ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
 
         # 2) ros2_control_node: loads hardware + controllers.yaml
         # Node(
